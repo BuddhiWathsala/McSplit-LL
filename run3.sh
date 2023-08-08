49383238737bd04dd003370ee90d2c8944997661
BuddhiWathsala <b.wathsala.bw@gmail.com>
2023-08-08 13:00:02 +1000

    Add more results

--- run3.sh
+++ run3.sh
@@ -15,7 +15,7 @@ write_output () {
 }
 
 task () {
-  out1=$(./bin/run.o $MCSP_HEURISTIC ./${1} ./${2} -l -q -t ${TIMEOUT})
+  out1=$(./bin/run.o $MCSP_HEURISTIC ./${1} ./${2} -l -q -t ${TIMEOUT} -c)
   out1="${1}, ${2}, ${out1}"
 #   out1=${out1//[,]/' '} 
   # echo $out1
@@ -28,8 +28,8 @@ task () {
   return
 }
 
-files=("si" "scalefree" "scalefree" "meshes-CVIU11" "LV" "largerLV" "images-CVIU11" "images-PR15" "biochemicalReactions")
-files=("largerLV")
+files=("largerLV" "si" "scalefree" "scalefree" "meshes-CVIU11" "LV" "images-CVIU11" "images-PR15" "biochemicalReactions")
+# files=("largerLV")
 for f in ${files[@]}; do
     file_name=indicators/${f}_data.txt
     results_file_name=results/${f}_${MCSP_HEURISTIC}_${TIMEOUT}.txt
